95a8c079d8e8c3a17ca7168e84c5aac478946e71
Crazzy-novus <duraivignesh222@gamil.com>
2023-12-17 22:42:25 +0530

    App ready to launch with level - 1 features

---
+++ android/app/src/main/kotlin/com/example/call_2_known/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.call_2_known
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,375 @@
+import 'dart:convert';
+import 'dart:math';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart' show Contact, FlutterContacts;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:fluttertoast/fluttertoast.dart' show Fluttertoast, Toast, ToastGravity;
+import 'dart:convert' as convert;
+
+
+// List to store contacts Global variable
+List<Contact> getContacts = [];
+
+List<SerializedContact>? _contacts;
+
+void main() {
+  runApp( const MaterialApp(
+    home: MyHomePage(),
+  )
+  );
+}
+
+/// Class to serialize a Contact
+class SerializedContact {
+  String? name;
+  String? phones;
+
+  SerializedContact({this.name, this.phones});
+
+  /// Converts this object to a JSON string
+  SerializedContact.fromJson(Map<String, dynamic> map)
+  : name = map['name'],
+    phones = map['phones'];
+  Map<String, dynamic> toJson(){
+    return {
+      'name': name,
+      'phones': phones,
+    };
+  }
+}
+
+/// Function to serialize a List<Contact>
+Future<List<SerializedContact>> serializeContacts(List<Contact> contacts) async {
+  List<SerializedContact> serializedContacts = [];
+  for (Contact contact in contacts) {
+    SerializedContact serializedContact = SerializedContact(
+      name: contact.displayName,
+      phones: contact.phones[0].number.isNotEmpty ? contact.phones[0].number: "0000000000",
+
+    );
+    serializedContacts.add(serializedContact);
+  }
+  return serializedContacts;
+}
+
+/// Function to get location to store contact file in memory
+Future<String> getExternalDocumentPath() async {
+  // To check whether permission is given for this app or not.
+  var status = await Permission.storage.status;
+  if (!status.isGranted) {
+    // If not we will ask for permission first
+    await Permission.storage.request();
+  }
+  final Directory directory = await getApplicationDocumentsDirectory();
+
+  return directory.path;
+}
+
+/// Function to fetch contacts from FlutterContacts if file is empty or does not exist
+Future<void> fetchContacts() async {
+  if (await FlutterContacts.requestPermission()) {
+    getContacts = await FlutterContacts.getContacts(
+      withProperties: true,
+    );
+    if (getContacts.isNotEmpty){
+      List<SerializedContact> contacts = await serializeContacts(getContacts);
+      _contacts = contacts;
+      Fluttertoast.showToast(
+        msg: 'Contacts loaded successfully',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.black87,
+        textColor: Colors.white,
+      );
+      saveToFile(contacts);
+
+    }
+    else{
+      Fluttertoast.showToast(
+        msg: 'Contacts not found',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.black87,
+        textColor: Colors.white,
+      );
+    }
+  }
+}
+
+/// Function to save contacts to file
+Future<void> saveToFile(List<SerializedContact> contacts) async {
+  try {
+    Directory? directory = await getDownloadsDirectory();
+    String filePath = '${directory?.path}/Contact.json';
+
+    File file = File(filePath);
+
+
+    final jsonString = jsonEncode(contacts);
+    await file.writeAsString(jsonString);
+
+
+    // Show toast message upon successful file creation
+  } catch (e) {
+    // Handle or rethrow the error as needed
+    Fluttertoast.showToast(
+      msg: 'error Contacts not stored in memory',
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.black87,
+      textColor: Colors.white,
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  void getPhoneData() async {
+
+    try {
+      Directory? directory = await getDownloadsDirectory();
+      String filePath = '${directory?.path}/Contact.json';
+      File file = File(filePath);
+
+      if (await file.exists()) {
+        final jsonString = await file.readAsString();
+        final list = convert.jsonDecode(jsonString) as List;
+        setState(() {
+          _contacts =
+              list.map((contactJson) => SerializedContact.fromJson(contactJson))
+                  .toList();
+        });
+
+
+        if (_contacts!.isNotEmpty) {
+          // File is not empty, try to parse contacts from the file
+
+          Fluttertoast.showToast(
+            msg: 'Contacts loaded Successfully',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.black87,
+            textColor: Colors.white,
+          );
+        } else {
+          // File is empty, fall back to getting contacts from FlutterContacts
+          await fetchContacts();
+        }
+      } else {
+        // File does not exist, fetch contacts using FlutterContacts
+        await fetchContacts();
+      }
+    } catch (e) {
+      // Handle or log the error as needed
+      Fluttertoast.showToast(
+        msg: 'Error in getting data from file',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.black87,
+        textColor: Colors.white,
+      );
+    }
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPhoneData();
+
+  }
+
+// Function to generate random number to get random contact to call
+  int getRandomContact(int totalContact) {
+    var random = Random();
+    return random.nextInt(totalContact);
+  }
+
+
+
+
+  // Function to make a phone call
+  void _makePhoneCall(String phoneNumber) async {
+    final url = 'tel:$phoneNumber';
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Call 2 Known",
+          style: TextStyle(color: Colors.blue,
+          fontSize: 30,
+          fontFamily: 'Roboto',
+          wordSpacing: 2.0,
+          fontWeight: FontWeight.bold,),
+
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center (
+        child: Column(
+          children: [
+            const SizedBox(height: 250),
+            Center(child: Row (
+              children: [
+                const SizedBox(width: 27.0),
+                TextButton(
+
+                  onPressed: () {
+                    int index = getRandomContact(_contacts!.length);
+                    _makePhoneCall(_contacts![index].phones!);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    // Background color of the button
+                    padding: const EdgeInsets.all(16),
+                    // Padding around the button content
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                    ),
+                  ),
+                  child: const Text(
+                    "Make a Call",
+                    style: TextStyle(
+                      color: Colors.yellow, // Text color of the button
+                      fontSize: 18, // Font size of the text
+                    ),),
+                ),
+                const SizedBox(width: 50.0),
+                ElevatedButton(
+
+                  onPressed: () {
+
+                    Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => const EditContactScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    // Background color of the button
+                    padding: const EdgeInsets.all(16),
+                    // Padding around the button content
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                    ),
+                  ),
+                  child: const Text(
+                    "Edit Contact",
+                    style: TextStyle(
+                      color: Colors.greenAccent, // Text color of the button
+                      fontSize: 18, // Font size of the text
+                    ),),
+                ),
+              ],
+            )),
+
+            const SizedBox(height: 75),
+            TextButton(
+
+              onPressed: () {
+                fetchContacts();
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.greenAccent,
+                // Background color of the button
+                padding: const EdgeInsets.all(16),
+                // Padding around the button content
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                ),
+              ),
+              child: const Text(
+                "Reset Contacts",
+                style: TextStyle(
+                  color: Colors.blue, // Text color of the button
+                  fontSize: 18, // Font size of the text
+                ),),
+            ),
+            ],
+        ),
+
+      ),
+    );
+  }
+}
+
+
+class EditContactScreen extends StatefulWidget {
+  const EditContactScreen({super.key});
+
+  @override
+  State<EditContactScreen> createState() => _EditContactScreenState();
+}
+
+class _EditContactScreenState extends State<EditContactScreen> {
+
+  void removeItem(int index) {
+    setState(() {
+      _contacts!.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Edit List",
+            style: TextStyle(color: Colors.blue),
+          ),
+          actions: [
+
+            IconButton(
+              icon: const Icon(Icons.save), // Icon you want to use
+              onPressed: () {
+                saveToFile(_contacts!);
+              },
+            ),
+            const SizedBox(width: 25.0),
+          ],
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: (_contacts == null) ? const Center(
+            child: CircularProgressIndicator()) : ListView.builder(
+            itemCount: _contacts!.length,
+            itemBuilder: (BuildContext, int index) {
+              String number = (_contacts![index].phones!.isNotEmpty) ? _contacts![index].phones! : "";
+              String name = (_contacts![index].name!.isNotEmpty) ? _contacts![index].name! : "";
+              return ListTile(
+                title: Text(name),
+                subtitle: Text(number),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () => removeItem(index),
+                ),
+              );
+            })
+    );
+  }
+}
